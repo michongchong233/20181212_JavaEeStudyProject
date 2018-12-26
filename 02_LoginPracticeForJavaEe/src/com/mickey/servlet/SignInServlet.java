acbff2476f24dd407a7e8282f720060b565ef027
Mickey Huang <a0909521623@gmail.com>
2018-12-26 12:12:15 +0800

    update SignInServlet code format

--- 02_LoginPracticeForJavaEe/src/com/mickey/servlet/SignInServlet.java
+++ 02_LoginPracticeForJavaEe/src/com/mickey/servlet/SignInServlet.java
@@ -61,30 +61,30 @@ public class SignInServlet extends BasicServlet {
 	 */
 	private void checkUserSignIn(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//獲取請求信息
-				String uname = (String)request.getParameter("uname");
-				String password = (String)request.getParameter("password");
-				//處理請求信息
-				UserService checkSignIn = new UserServiceImpl();
-				UserDTO user = checkSignIn.checkUserSignIn(uname, password);
-				if (user != null) {
-					//如果有勾選Remenber Me就將uid存儲至Cookie中
-					String remenberMe = request.getParameter("remenberMe");
-					if(remenberMe != null && remenberMe.equals("remenberMe")) {
-						Cookie userCookie = new Cookie("uid", Integer.toString(user.getUid()));
-						userCookie.setMaxAge(3*24*3600);//設置Cookie有效期限
-						response.addCookie(userCookie);
-					}
-					
-					//獲取Session
-					HttpSession hs = request.getSession();
-					//將用戶信息寫入Session中
-					hs.setAttribute("user", user);
-					
-					//登入成功，重定向至dashboard
-					response.sendRedirect("DashBoard.jsp");
-				}else{
-					request.setAttribute("errorMessage", "error");
-					request.getRequestDispatcher("SignIn.jsp").forward(request, response);
-				}
+		String uname = (String)request.getParameter("uname");
+		String password = (String)request.getParameter("password");
+		//處理請求信息
+		UserService checkSignIn = new UserServiceImpl();
+		UserDTO user = checkSignIn.checkUserSignIn(uname, password);
+		if (user != null) {
+			//如果有勾選Remenber Me就將uid存儲至Cookie中
+			String remenberMe = request.getParameter("remenberMe");
+			if(remenberMe != null && remenberMe.equals("remenberMe")) {
+				Cookie userCookie = new Cookie("uid", Integer.toString(user.getUid()));
+				userCookie.setMaxAge(3*24*3600);//設置Cookie有效期限
+				response.addCookie(userCookie);
+			}
+			
+			//獲取Session
+			HttpSession hs = request.getSession();
+			//將用戶信息寫入Session中
+			hs.setAttribute("user", user);
+			
+			//登入成功，重定向至dashboard
+			response.sendRedirect("DashBoard.jsp");
+		}else{
+			request.setAttribute("errorMessage", "error");
+			request.getRequestDispatcher("SignIn.jsp").forward(request, response);
+		}
 	}
 }
